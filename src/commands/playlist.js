db8f1020d34fff0031d3652ed453943c4bba322e
juampapo546 <juampicarosi@gmail.com>
2021-08-30 10:39:01 -0300

    playlist: Handle all promise rejections

--- src/commands/playlist.js
+++ src/commands/playlist.js
@@ -7,38 +7,41 @@ module.exports = {
   name: "playlist",
   description: "queue youtube playlists",
   execute(args, msg, KEYS) {
-    const youtube = new Youtube(KEYS.youtube1);
+    const youtube = new Youtube(KEYS.youtube2);
     return new Promise((resolve, reject) => {
       try {
-        youtube.getPlaylist(String(args)).then((playlist) => {
-          console.log(`The playlist's title is ${playlist.title}`);
-          playlist.getVideos().then(async (videos) => {
-            for (let i = 0; i < videos.length; i++) {
-              list.push({
-                link: `https://www.youtube.com/watch?v=${videos[i].id}`,
-                title: videos[i].title,
-              });
-            }
-
-            await Promise.all(
-              list.map((video) => {
-                return new Promise(async (resolve, reject) => {
-                  const data = await yt.getBasicInfo(video.link);
-                  video.duration = parseInt(data.videoDetails.lengthSeconds);
-                  resolve();
+        youtube
+          .getPlaylist(String(args))
+          .then((playlist) => {
+            console.log(`The playlist's title is ${playlist.title}`);
+            playlist.getVideos().then(async (videos) => {
+              for (let i = 0; i < videos.length; i++) {
+                list.push({
+                  link: `https://www.youtube.com/watch?v=${videos[i].id}`,
+                  title: videos[i].title,
                 });
-              })
-            );
+              }
 
-            msg.channel.send(
-              `This playlist has ${
-                videos.length === 50 ? "50+" : videos.length
-              } videos.`
-            );
-            console.log(list);
-            resolve(list);
-          });
-        });
+              await Promise.all(
+                list.map((video) => {
+                  return new Promise(async (resolve, reject) => {
+                    const data = await yt.getBasicInfo(video.link);
+                    video.duration = parseInt(data.videoDetails.lengthSeconds);
+                    resolve();
+                  });
+                })
+              ).catch((error) => console.log(error));
+
+              msg.channel.send(
+                `This playlist has ${
+                  videos.length === 50 ? "50+" : videos.length
+                } videos.`
+              );
+              console.log(list);
+              resolve(list);
+            });
+          })
+          .catch((error) => console.log(error));
       } catch (error) {
         reject(error);
       }
